6f17a52f818417dd430b07fe95cff165fca49967
Janusz13 <janusz.baranowski.2@gmail.com>
2015-08-31 22:29:21 +0200

    Brave

---
+++ cpptest/src/cpptest.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : cpptest.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Brave World!!!" << endl; // prints !!!Hello World!!!
+	double a,b;
+	char type;
+	while (cin) {
+
+		cout << "a: ";
+		cin >> a;
+		cout << "b: ";
+		cin >> b;
+		complex<double> c (a,b);
+		cout << "c = " << c << "\nContinue [y/n]?";
+		cin >> type;
+		if (type != 'y') break;
+	}
+
+	return 0;
+}
